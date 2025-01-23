40a0b560c0d13d9aeef560af7d439fa972679375
LukasVGP <hoplite.arts@gmail.com>
2025-01-22 22:42:17 -0800

    movement work

--- Assets/Scribts/ZombieBody.cs
+++ Assets/Scribts/ZombieBody.cs
@@ -2,75 +2,74 @@ using UnityEngine;
 
 public class ZombieBody : MonoBehaviour
 {
+    private MonoBehaviour currentCarrier;
+    private bool isBeingCarried = false;
+
     [Header("Grab Points")]
     public Transform headPosition;
     public Transform feetPosition;
-
-    private MonoBehaviour headPlayer;
-    private MonoBehaviour feetPlayer;
-    private bool isBeingCarried = false;
+    [SerializeField] private float grabRadius = 0.5f;
 
     private void OnTriggerStay2D(Collider2D other)
     {
+        if (isBeingCarried) return;
+
         var redPlayer = other.GetComponent<RedPlayerController>();
         var bluePlayer = other.GetComponent<BluePlayerController>();
 
         if (redPlayer != null && redPlayer.WantsToGrab)
         {
-            TryGrab(redPlayer);
+            CheckGrabPoints(redPlayer);
         }
-        else if (bluePlayer != null && bluePlayer.WantsToGrab)
+        if (bluePlayer != null && bluePlayer.WantsToGrab)
         {
-            TryGrab(bluePlayer);
+            CheckGrabPoints(bluePlayer);
+        }
+    }
+
+    private void CheckGrabPoints(MonoBehaviour player)
+    {
+        float distanceToHead = Vector2.Distance(player.transform.position, headPosition.position);
+        float distanceToFeet = Vector2.Distance(player.transform.position, feetPosition.position);
+
+        if (distanceToHead <= grabRadius || distanceToFeet <= grabRadius)
+        {
+            TryGrab(player);
         }
     }
 
     private void TryGrab(MonoBehaviour player)
     {
-        float distanceToHead = Vector2.Distance(player.transform.position, headPosition.position);
-        float distanceToFeet = Vector2.Distance(player.transform.position, feetPosition.position);
+        currentCarrier = player;
+        isBeingCarried = true;
+        transform.parent = player.transform;
 
-        if (distanceToHead < 1f && headPlayer == null)
-        {
-            headPlayer = player;
-            if (player is RedPlayerController redPlayer) redPlayer.GrabBody(this);
-            if (player is BluePlayerController bluePlayer) bluePlayer.GrabBody(this);
-        }
-        else if (distanceToFeet < 1f && feetPlayer == null)
-        {
-            feetPlayer = player;
-            if (player is RedPlayerController redPlayer) redPlayer.GrabBody(this);
-            if (player is BluePlayerController bluePlayer) bluePlayer.GrabBody(this);
-        }
-
-        isBeingCarried = (headPlayer != null && feetPlayer != null);
+        if (player is RedPlayerController redPlayer) redPlayer.GrabBody(this);
+        if (player is BluePlayerController bluePlayer) bluePlayer.GrabBody(this);
     }
 
     public void Release(MonoBehaviour player)
     {
-        if (player == headPlayer) headPlayer = null;
-        if (player == feetPlayer) feetPlayer = null;
-        isBeingCarried = false;
+        if (player == currentCarrier)
+        {
+            currentCarrier = null;
+            isBeingCarried = false;
+            transform.parent = null;
+        }
     }
 
     public void OnDropped()
     {
-        headPlayer = null;
-        feetPlayer = null;
+        currentCarrier = null;
         isBeingCarried = false;
         transform.parent = null;
     }
 
     private void Update()
     {
-        if (isBeingCarried)
+        if (isBeingCarried && currentCarrier != null)
         {
-            Vector3 midPoint = (headPlayer.transform.position + feetPlayer.transform.position) / 2f;
-            transform.position = midPoint;
-
-            Vector3 direction = (headPlayer.transform.position - feetPlayer.transform.position).normalized;
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(0, 0, angle);
+            transform.position = currentCarrier.transform.position;
         }
     }
 
